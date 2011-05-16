3348e2f527cab43b60a00d53f630f7a4fe56d47f
hejin <hejin1026@gmail.com>
2011-05-16 19:40:41 +0800

    hejin

--- telnet/src/olt_telnet.erl
+++ telnet/src/olt_telnet.erl
@@ -1,34 +1,19 @@
--module(telnet_conn).
+-module(olt_telnet).
 
--author("hejin-2011-4-27").
+-author("hejin-2011-5-16").
 
--behaviour(gen_server).
-
--export([start_link/1, start/1,
-        get_data/2, get_data/3,
-        send_data/2,
-        stop/1]).
-
-%% Callback
--export([init/1,
-        handle_call/3,
-        handle_cast/2,
-        handle_info/2,
-        terminate/2,
-        code_change/3]).
+-export([start/1,
+        get_data/2,
+        send_data/2
+        ]).
 
 -include("elog.hrl").
 
 -define(CONN_TIMEOUT, 10000).
--define(CALL_TIMEOUT, 12000).
 
--define(username,"login: ").
--define(password,"Password: ").
--define(prx,"login: |Password: |\\\$ |> ").
-
-%-define(username,"Username:").
-%-define(password,"Password:").
-%-define(prx,"Username:|Password:|\\\#|> ").
+-define(username,"Username:").
+-define(password,"Password:").
+-define(prx,"Username:|Password:|\\\#|> ").
 
 -define(keepalive, true).
 
@@ -40,11 +25,11 @@
 	  socket,
 	  conn_state
 	 }).
-     
+
 
 %%%%%%%%%%%%%%%%%%% extra interface %%%%%%%%%%%%%%%%%
 start(Opts) ->
-    case start_link(Opts) of
+    case init(Opts) of
     {ok, Pid} ->
         {ok, Pid};
     {error, _Error} ->
@@ -52,24 +37,17 @@ start(Opts) ->
     end.
 
 get_data(Pid, Cmd) ->
-    get_data(Pid, Cmd, ?CALL_TIMEOUT).
-
-get_data(Pid, Cmd, Timeout)  ->
-    gen_server:call(Pid, {cmd, Cmd}, Timeout).
+    telnet_client:send_data(Pid, Cmd),
+    Resp = telnet_client:get_data(Pid),
+    {ok, Resp}.
 
 send_data(Pid, Cmd) ->
-    gen_server:cast(Pid, {cmd, Cmd}).
-
-stop(Pid) ->
-    gen_server:call(Pid, stop).
+     telnet_client:send_data(Pid, Cmd),
+     ok.
 
 
 
-start_link(Opts) ->
-    gen_server:start_link(?MODULE, [Opts], []).
-
-
-init([Opts]) ->
+init(Opts) ->
     io:format("starting telnet conn ...~p",[Opts]),
     Host = proplists:get_value(host, Opts, "localhost"),
     Port = proplists:get_value(port, Opts, 23),
@@ -124,42 +102,3 @@ connect(Ip,Port,Timeout,KeepAlive,Username,Password) ->
                             Error
 	end,
     Result.
-
-
-handle_call(stop, _From, State) ->
-    ?INFO("received stop request", []),
-    {stop, normal, State};
-
-handle_call({cmd, Cmd}, From, #state{conn_state = connected, socket = Socket} = State) ->
-    ?INFO("handle_call,from:~p, Cmd,~p", [From, Cmd]),
-    telnet_client:send_data(Socket, Cmd),
-    Resp = telnet_client:get_data(Socket),
-    ?INFO("respond :~p",[Resp]),
-    {reply, Resp, State};
-
-handle_call({cmd, _Cmd}, _From, #state{conn_state = ConnState} = State) ->
-    {reply, {error, {conn_state, ConnState}}, State};
-
-handle_call(Req, _From, State) ->
-    ?WARNING("unexpect request: ~p", [Req]),
-    {reply, {error, {invalid_request, Req}}, State}.
-
-
-handle_cast({cmd, Cmd}, #state{conn_state = connected, socket = Socket} = State) ->
-    ?INFO("handle_cast, Cmd,~p", [Cmd]),
-    telnet_client:send_data(Socket, Cmd),
-    {noreply, State};
-
-handle_cast(Req, State) ->
-    ?WARNING("unexpected cast: ~n~p, state:~p", [Req, State]),
-    {noreply, State}.
-
-handle_info(Info, State) ->
-    ?WARNING("unexpected info: ~n~p", [Info]),
-    {noreply, State}.
-
- terminate(_Reason, _State) ->
-    ok.
-
-code_change(_Vsn, State, _Extra) ->
-    {ok, State}.
