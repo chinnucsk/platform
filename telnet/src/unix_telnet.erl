dfdad5d0a658d2d43e2a19bad578727c9b4e3c24
hejin <hejin1026@gmail.com>
2011-05-19 15:02:04 +0800

    hejin

--- telnet/src/unix_telnet.erl
+++ telnet/src/unix_telnet.erl
@@ -1,109 +1,75 @@
--module(telnet_conn).
+-module(unix_telnet).
+-compile(export_all).
 
--author("hejin-2011-4-27").
-
--export([start/1,
-        get_data/2, 
-        send_data/2
-        ]).
-
--include("elog.hrl").
-
--define(CONN_TIMEOUT, 10000).
+-export([connect/6,get_prompt_regexp/0]).
 
 -define(username,"login: ").
 -define(password,"Password: ").
 -define(prx,"login: |Password: |\\\$ |> ").
 
--define(keepalive, true).
-
--record(state, {
-	  host,
-	  port,
-	  user,
-	  password,
-	  socket,
-	  conn_state
-	 }).
-     
-
-%%%%%%%%%%%%%%%%%%% extra interface %%%%%%%%%%%%%%%%%
-start(Opts) ->
-    case init(Opts) of
-    {ok, Pid} ->
-        {ok, Pid};
-    {error, _Error} ->
-        {ok, undefined}
-    end.
-
-get_data(Pid, Cmd) ->
-    telnet_client:send_data(Pid, Cmd),
-    Resp = telnet_client:get_data(Pid),
-    {ok, Resp}.
-
-
-send_data(Pid, Cmd) ->
-     telnet_client:send_data(Pid, Cmd),
-     ok.
-
- stop(Pid) ->
-     telnet_client:close(Pid),
-     ok.
-
-
-
-init(Opts) ->
-    io:format("starting telnet conn ...~p",[Opts]),
-    Host = proplists:get_value(host, Opts, "localhost"),
-    Port = proplists:get_value(port, Opts, 23),
-    Username = proplists:get_value(username, Opts, "root"),
-    Password = proplists:get_value(password, Opts, "public"),
-    case (catch connect(Host, Port, ?CONN_TIMEOUT, ?keepalive, Username, Password)) of
-	{ok, Pid} ->
-	    {ok, #state{host = Host, port = Port, socket = Pid, user = Username, password = Password, conn_state = connected}};
-	{error, Error} ->
-	    {stop, Error};
-    {'EXIT', Reason} ->
-        {stop, Reason}
-	end.
+%%%-----------------------------------------------------------------
+%%% @hidden
+%%% @spec get_prompt_regexp() -> PromptRegexp
+%%%      PromptRegexp = ct_telnet:prompt_regexp()
+%%%
+%%% @doc Callback for ct_telnet.erl.
+%%%
+%%% <p>Return the prompt regexp for telnet connections to the
+%%% interwatch instrument.</p>
+get_prompt_regexp() ->
+    ?prx.
+%%%-----------------------------------------------------------------
+%%% @hidden
+%%% @spec connect(Ip,Port,Timeout,Extra) -> {ok,Handle} | {error,Reason}
+%%%      Ip = string() | {integer(),integer(),integer(),integer()}
+%%%      Port = integer()
+%%%      Timeout = integer()
+%%%      Extra = {Username,Password}
+%%%      Username = string()
+%%%      Password = string()
+%%%      Handle = ct_telnet:handle()
+%%%
+%%% @doc Callback for ct_telnet.erl.
+%%%
+%%% <p>Setup telnet connection to a UNIX host.</p>
 
 connect(Ip,Port,Timeout,KeepAlive,Username,Password) ->
-    ?INFO("telnet:connect",[]),
-    Result =case telnet_client:open(Ip,Port,Timeout,KeepAlive) of
-                {ok,Pid} ->
-                    ?INFO("open success...~p",[Pid]),
-                    case telnet:silent_teln_expect(Pid,[],[prompt],?prx,[]) of
-                        {ok,{prompt,?username},_} ->
-                        ok = telnet_client:send_data(Pid,Username),
-                        ?INFO("Username: ~s",[Username]),
-                        case telnet:silent_teln_expect(Pid,[],prompt,?prx,[]) of
-                            {ok,{prompt,?password},_} ->
-                            ok = telnet_client:send_data(Pid,Password),
-%                            Stars = lists:duplicate(length(Password),$*),
-                            ?INFO("Password: ~s",[Password]),
-                            ok = telnet_client:send_data(Pid,""),
-                            case telnet:silent_teln_expect(Pid,[],prompt,
-                                              ?prx,[]) of
-                                {ok,{prompt,Prompt},_}
-                                when Prompt=/=?username, Prompt=/=?password ->
-                                {ok,Pid};
-                                Error ->
-                                ?WARNING("Password failed\n~p\n",
-                                     [Error]),
-                                {error,Error}
-                            end;
-                            Error ->
-                            ?WARNING("Login failed\n~p\n",[Error]),
-                            {error,Error}
-                        end;
-                        {ok,[{prompt,_OtherPrompt1},{prompt,_OtherPrompt2}],_} ->
-                        {ok,Pid};
-                        Error ->
-                        ?WARNING("Did not get expected prompt\n~p\n",[Error]),
-                        {error,Error}
-                    end;
-                Error ->
-                    ?WARNING("Could not open telnet connection\n~p\n",[Error]),
-                            Error
+    io:format("unix_telnet:connect~n"),
+    Result = 
+	case telnet_client:open(Ip,Port,Timeout,KeepAlive) of
+            {ok,Pid} ->
+		case telnet:silent_teln_expect(Pid,[],[prompt],?prx,[]) of
+		    {ok,{prompt,?username},_} ->
+			ok = telnet_client:send_data(Pid,Username),
+			io:format("Username: ~s~n",[Username]),
+			case telnet:silent_teln_expect(Pid,[],prompt,?prx,[]) of
+			    {ok,{prompt,?password},_} ->
+				ok = telnet_client:send_data(Pid,Password),
+				Stars = lists:duplicate(length(Password),$*),
+				io:format("Password: ~s~n",[Stars]),
+				ok =telnet_client:send_data(Pid,""),
+				case telnet:silent_teln_expect(Pid,[],prompt,
+								  ?prx,[]) of
+				    {ok,{prompt,Prompt},_} 
+				    when Prompt=/=?username, Prompt=/=?password ->
+					{ok,Pid};
+				    Error ->
+					io:format("Password failed\n~p\n",
+						 [Error]),
+					{error,Error}
+				end;
+			    Error ->
+				io:format("Login failed\n~p\n",[Error]),
+				{error,Error}
+			end;
+		    {ok,[{prompt,_OtherPrompt1},{prompt,_OtherPrompt2}],_} ->
+			{ok,Pid};
+		    Error ->
+			io:format("Did not get expected prompt\n~p\n",[Error]),
+			{error,Error}
+		end;
+	    Error ->
+		io:format("Could not open telnet connection\n~p\n",[Error]),
+		Error
 	end,
     Result.
