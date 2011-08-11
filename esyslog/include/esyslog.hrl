ea8cf8712353c004da3bfe0bb4da074bd7822347
zzdhidden <zzdhidden@gmail.com>
2011-08-11 19:57:49 +0800

    update include

--- esyslog/include/esyslog.hrl
+++ esyslog/include/esyslog.hrl
@@ -1,7 +1,11 @@
 
+-define(SYSLOG_CRIT(Format, Args), esyslog:crit(?MODULE, ?LINE, Format, Args)).
+
 -define(SYSLOG_ERR(Format, Args), esyslog:error(?MODULE, ?LINE, Format, Args)).
 
 -define(SYSLOG_WARN(Format, Args), esyslog:warn(?MODULE, ?LINE, Format, Args)).
 
 -define(SYSLOG_INFO(Format, Args), esyslog:info(?MODULE, ?LINE, Format, Args)).
+
+-define(SYSLOG_DEBUG(Format, Args), esyslog:debug(?MODULE, ?LINE, Format, Args)).
 
